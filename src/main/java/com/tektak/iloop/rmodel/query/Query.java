8c3ed61ced199f45512703b37aab5e42cdb989c9
julina.maharjan <julina.maharjan@tektak.com>
2014-08-01 10:55:51 +0545

    Method for Callable statement and generatedId added

--- src/main/java/com/tektak/iloop/rmodel/query/Query.java
+++ src/main/java/com/tektak/iloop/rmodel/query/Query.java
@@ -3,6 +3,7 @@ package com.tektak.iloop.rmodel.query;
 import com.tektak.iloop.rmodel.RmodelException;
 import com.tektak.iloop.rmodel.driver.Sql;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -12,9 +13,14 @@ import java.sql.ResultSet;
  */
 public interface Query {
     public void InitPreparedStatement() throws RmodelException.SqlException, RmodelException.CommonException;
+    public void InitPreparedStatement(int type) throws RmodelException.SqlException, RmodelException.CommonException;
+    public void InitCallableStatement() throws RmodelException.SqlException, RmodelException.CommonException;
     public PreparedStatement getPreparedStatement();
     public void setPreparedStatement(PreparedStatement preparedStatement);
+    public CallableStatement getCallableStatement();
+    public void setCallableStatement(CallableStatement callableStatement);
     public int Dml() throws RmodelException.SqlException;
+    public int DmlWithGeneratedId() throws RmodelException.SqlException;
     public ResultSet Drl() throws RmodelException.SqlException;
     public void setSql(Sql sql);
     public void Close() throws RmodelException.SqlException;
